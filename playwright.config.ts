795785a59a0f651e898c450ff857119a2c952c85
TatianaInzhee <tatiana@fastforward.ai>
2025-02-28 21:20:27 +0100

    Adding reports to config files

--- playwright.config.ts
+++ playwright.config.ts
@@ -61,7 +61,6 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
